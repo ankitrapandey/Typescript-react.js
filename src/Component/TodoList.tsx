ae5e42615b3bef2549e48b8bc9f54b703d7fc9a9
AnkirPandey <developerankitpandey@gmail.com>
2024-06-25 10:59:59 +0530

    to-do-list

--- src/Component/TodoList.tsx
+++ src/Component/TodoList.tsx
@@ -1,16 +1,92 @@
-import  { Component, ChangeEvent } from 'react';
+// import  { Component, ChangeEvent } from 'react';
+// import { Container, Typography, Divider, TextField, Button, ListItem, ListItemText, IconButton } from '@mui/material';
+// import { Delete as DeleteIcon } from '@mui/icons-material';
+
+// interface State {
+//   listTodo: string[];
+//   inputText: string;
+// }
+
+// class TodoList extends Component<{}, State> {
+//   state: State = {
+//     listTodo: [],
+//     inputText: ''
+//   };
+
+//   inputChange = (e: ChangeEvent<HTMLInputElement>) => {
+//     this.setState({ inputText: e.target.value });
+//   };
+
+//   addItem = () => {
+//     const { inputText, listTodo } = this.state;
+//       this.setState({
+//         listTodo: [...listTodo, inputText],
+//         inputText: ''
+//       });
+  
+//   };
+
+//   deleteItem = (tarindex: number) => {
+//     this.state.listTodo = this.state.listTodo.filter((_, index) => index !== tarindex)
+//     this.setState({ listTodo: [...this.state.listTodo] })
+//   };
+
+//   render() {
+//     const { listTodo, inputText } = this.state;
+
+//     return (
+//       <Container className="main-container" style={{ marginTop: '20px' }}>
+//         <div className="center-container">
+//           <Typography variant="h4" className="app-heading" style={{ textAlign: 'center' }}>TODO</Typography>
+//           <Divider style={{ marginBottom: '20px' }} />
+//           <div className="input-container" style={{ marginBottom: '20px' }}>
+//             <TextField
+//               label="Enter your todo item"
+//               value={inputText}
+//               onChange={this.inputChange}
+//               fullWidth
+//             />
+//             <Button
+//               variant="contained"
+//               color="primary"
+//               onClick={this.addItem}
+//               style={{ marginTop: '10px' }}
+//             >
+//               Add
+//             </Button>
+//           </div>
+//           {listTodo.map((listItem, index) => (
+//             <ListItem className="list-item" key={index} style={{ display: 'flex', justifyContent: 'space-between' }}>
+//               <ListItemText primary={listItem} />
+//               <IconButton edge="end" aria-label="delete" onClick={() => this.deleteItem(index)}>
+//                 <DeleteIcon />
+//               </IconButton>
+//             </ListItem>
+//           ))}
+//         </div>
+//       </Container>
+//     );
+//   }
+// }
+
+// export default TodoList;
+
+
+import { Component, ChangeEvent } from 'react';
 import { Container, Typography, Divider, TextField, Button, ListItem, ListItemText, IconButton } from '@mui/material';
-import { Delete as DeleteIcon } from '@mui/icons-material';
+import { Delete as DeleteIcon, Edit as EditIcon } from '@mui/icons-material';
 
 interface State {
   listTodo: string[];
   inputText: string;
+  editIndex: number | null;
 }
 
 class TodoList extends Component<{}, State> {
   state: State = {
     listTodo: [],
-    inputText: ''
+    inputText: '',
+    editIndex: null
   };
 
   inputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -18,17 +94,31 @@ class TodoList extends Component<{}, State> {
   };
 
   addItem = () => {
-    const { inputText, listTodo } = this.state;
+    const { inputText, listTodo, editIndex } = this.state;
+    if (editIndex !== null) {
+      listTodo[editIndex] = inputText;
+      this.setState({ listTodo, inputText: '', editIndex: null });
+    } else {
       this.setState({
         listTodo: [...listTodo, inputText],
         inputText: ''
       });
-  
+    }
   };
 
   deleteItem = (tarindex: number) => {
-    this.state.listTodo = this.state.listTodo.filter((_, index) => index !== tarindex)
-    this.setState({ listTodo: [...this.state.listTodo] })
+    const { listTodo } = this.state;
+    this.setState({
+      listTodo: listTodo.filter((_, index) => index !== tarindex)
+    });
+  };
+
+  editItem = (index: number) => {
+    const { listTodo } = this.state;
+    this.setState({
+      inputText: listTodo[index],
+      editIndex: index
+    });
   };
 
   render() {
@@ -52,15 +142,20 @@ class TodoList extends Component<{}, State> {
               onClick={this.addItem}
               style={{ marginTop: '10px' }}
             >
-              Add
+              {this.state.editIndex !== null ? 'Edit' : 'Add'}
             </Button>
           </div>
           {listTodo.map((listItem, index) => (
             <ListItem className="list-item" key={index} style={{ display: 'flex', justifyContent: 'space-between' }}>
               <ListItemText primary={listItem} />
-              <IconButton edge="end" aria-label="delete" onClick={() => this.deleteItem(index)}>
-                <DeleteIcon />
-              </IconButton>
+              <div>
+                <IconButton edge="end" aria-label="edit" onClick={() => this.editItem(index)}>
+                  <EditIcon />
+                </IconButton>
+                <IconButton edge="end" aria-label="delete" onClick={() => this.deleteItem(index)}>
+                  <DeleteIcon />
+                </IconButton>
+              </div>
             </ListItem>
           ))}
         </div>
@@ -70,4 +165,3 @@ class TodoList extends Component<{}, State> {
 }
 
 export default TodoList;
-
